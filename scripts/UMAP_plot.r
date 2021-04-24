19b42e513e6dd5a0e96aa6229bd5587984092e74
WeiWei060512 <weiwei060512@gmail.com>
2021-04-24 20:20:09 +0100

    added R library

--- scripts/UMAP_plot.r
+++ scripts/UMAP_plot.r
@@ -1,4 +1,5 @@
 library(M3C)
+library(umap)
 
 ##### UMAP plots ######
 output_dir <- './'
@@ -6,8 +7,8 @@ donor <- 'eipl_1'
 
 filePath <- paste0(output_dir, donor, 'cluster.rds')
 mydonor <- readRDS(filePath)
-donor <- gsub('.*/','',filePath)
-donor <- gsub('cluster.rds','',donor)
+#donor <- gsub('.*/','',filePath)
+#donor <- gsub('cluster.rds','',donor)
 
 pdf(paste0(output_dir, donor, '_sumHF.cluster.pdf')) # width=8, height=5
 umap(mydonor$data,labels=(mydonor$sumHF),controlscale=TRUE,scale=1,dotsize=3,printheight = 15, printwidth = 22)
